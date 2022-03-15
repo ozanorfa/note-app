9be89d5208e3b4ba5f66d7924ff5df91f0966a52
oorfa <ozan.orfa@golive.com.tr>
2022-03-15 19:09:16 +0300

    added Libraries and Splash Screen

--- app/src/main/java/com/task/noteapp/MainActivity.kt
+++ app/src/main/java/com/task/noteapp/MainActivity.kt
@@ -2,13 +2,31 @@ package com.task.noteapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.task.noteapp.ui.splash.SplashFragment
 
 class MainActivity : AppCompatActivity() {
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //İnitial Commit of Fragment
+        addFragment(SplashFragment(), "TAG")
+    }
+
+    private fun addFragment(fragment: Fragment, tag: String) {
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container_view, fragment, tag)
+            .addToBackStack("")
+            .commit()
+    }
+
+    fun replaceFragment(fragment: Fragment, tag: String) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container_view, fragment, tag)
+            .addToBackStack("")
+            .commit()
     }
 }
